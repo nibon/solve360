5fcde69fc1cc344896570c082e2da85947c41755
Daniel Nibon <daniel@nibon.se>
2014-08-09 15:52:06 +0200

    Updated version to 0.9.1

--- solve360/__init__.py
+++ solve360/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 """Python Solve360 API Wrapper."""
 
-__version__ = '0.9.0'
+__version__ = '0.9.1'
 __all__ = ['Solve360']
 
 from solve360.solve360 import Solve360
